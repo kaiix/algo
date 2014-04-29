485a9bc9af8e565b26dca2f43cacd1b65abaf859
kaiix <kvn.hou@gmail.com>
2014-04-29 23:29:43 +0800

    Fix test case

--- kaiix/tree/NumTrees.java
+++ kaiix/tree/NumTrees.java
@@ -26,6 +26,6 @@ public class NumTrees {
 
     public static void main(String[] args) {
         for (int i = 0; i < 12; i++)
-            System.out.println(numTrees());
+            System.out.println(numTrees(i));
     }
 }
