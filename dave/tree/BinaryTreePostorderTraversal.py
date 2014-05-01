a51825932f1c5a5667334a9ed6e93f694002f950
David Chen <dave.chen.ww@gmail.com>
2014-05-01 10:57:51 +0800

    Solve "Binary Tree Postorder Traversal"

---
+++ dave/tree/BinaryTreePostorderTraversal.py
@@ -0,0 +1,16 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    # @param root, a tree node
+    # @return a list of integers
+    def postorderTraversal(self, root):
+        if not root:
+            return []
+        return self.postorderTraversal(root.left) + \
+            self.postorderTraversal(root.right) + [root.val]
