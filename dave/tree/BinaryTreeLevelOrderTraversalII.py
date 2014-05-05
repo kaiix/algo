ab0d3aed01dc5be3440e43826119d6bc458e4d93
David Chen <dave.chen.ww@gmail.com>
2014-05-05 23:08:32 +0800

    Solve "Binary Tree Level Order Traversal II"

---
+++ dave/tree/BinaryTreeLevelOrderTraversalII.py
@@ -0,0 +1,29 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    # @param root, a tree node
+    # @return a list of lists of integers
+    def levelOrderBottom(self, root):
+        q = [[root]]
+        ret = []
+        while q:
+            l = q.pop(0)
+            v = []
+            ll = []
+            for n in l:
+                if n:
+                    v.append(n.val)
+                    ll.append(n.left)
+                    ll.append(n.right)
+            if v:
+                ret.append(v)
+            if ll:
+                q.append(ll)
+        ret.reverse()
+        return ret
