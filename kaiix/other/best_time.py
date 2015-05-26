b1f97e8e80e88a5d451e1d3ec03fad87b2ce25f0
kaiix <kvn.hou@gmail.com>
2015-05-26 22:29:14 +0800

    Update "Best Time to Buy and Sell Stock", solve it from start position

--- kaiix/other/best_time.py
+++ kaiix/other/best_time.py
@@ -14,7 +14,23 @@ def maxProfit(prices):
     return maxProfit
 
 
+def maxProfitFromStart(prices):
+    if len(prices) <= 1:
+        return 0
+
+    minPrice = 0x7fffff
+    maxProfit = 0
+    for i in xrange(len(prices)):
+        minPrice = min(minPrice, prices[i])
+        maxProfit = max(maxProfit, prices[i] - minPrice)
+    return maxProfit
+
+
 if __name__ == '__main__':
     print maxProfit([1, 2])
     print maxProfit([1, 2, 3, 4, 5])
     print maxProfit([5, 8, 3, 7, 2, 6, 10, 30, 1, 28])
+
+    print maxProfitFromStart([1, 2])
+    print maxProfitFromStart([1, 2, 3, 4, 5])
+    print maxProfitFromStart([5, 8, 3, 7, 2, 6, 10, 30, 1, 28])
