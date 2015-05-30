79b88483e6d58af5b19a28deeef0f06f4598f344
kaiix <kvn.hou@gmail.com>
2015-05-30 21:45:42 +0800

    Update "Word Break", use operater `in`

--- kaiix/dp/word_break.py
+++ kaiix/dp/word_break.py
@@ -6,26 +6,20 @@ def wordBreak(s, wordDict):
         return len(wordDict) == 0
 
     dp = [False] * len(s)
-    for i in xrange(len(s)):
-        if i == 0:
-            for w in wordDict:
-                if w == s[0]:
-                    dp[0] = True
-                    break
-        else:
-            for j in reversed(xrange(i)):
-                if dp[j]:
-                    substr = s[j+1:i+1]
-                else:
-                    substr = s[:i+1]
-                for w in wordDict:
-                    if substr == w:
-                        dp[i] = True
-                        break
-                if dp[i]:
-                    break
-    return dp[-1]
 
+    for i in xrange(len(s)):
+        substr = s[:i+1]
+        if substr in wordDict:
+            dp[i] = True
+            continue
+        for j in reversed(xrange(i)):
+            if dp[j]:
+                substr = s[j+1:i+1]
+            if substr in wordDict:
+                dp[i] = True
+                break
+
+    return dp[-1]
 
 if __name__ == '__main__':
     print wordBreak('leetcode', ['leet', 'code'])
