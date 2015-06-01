65efbc4477ac37f5143609cb8203a07452d03a22
kaiix <kvn.hou@gmail.com>
2015-06-01 22:07:56 +0800

    Update "Gray Code"

--- kaiix/other/gray_code.py
+++ kaiix/other/gray_code.py
@@ -4,6 +4,16 @@
 def grayCode(n):
     result = [0]
     for i in xrange(n):
+        msb = 1 << i
+        for j in reversed(xrange(len(result))):
+            result.append(result[j] | msb)
+    return result
+
+
+# a bit slower
+def grayCode2(n):
+    result = [0]
+    for i in xrange(n):
         reflected = []
         msb = 1 << i
         for j in reversed(result):
@@ -11,8 +21,10 @@ def grayCode(n):
         result.extend(reflected)
     return result
 
+
 #  http://en.wikipedia.org/wiki/Gray_code
 #  Gray code, aka "reflected binary code"
 
 if __name__ == '__main__':
     print grayCode(2)
+    print grayCode(3)
