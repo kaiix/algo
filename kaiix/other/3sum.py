30b9e68b576a8172954b5242343ce307d5bd615e
kaiix <kvn.hou@gmail.com>
2015-08-03 23:04:33 +0800

    Update solution of "3Sum"

--- kaiix/other/3sum.py
+++ kaiix/other/3sum.py
@@ -2,39 +2,29 @@
 
 
 def threeSum(nums):
-    result = []
     nums = sorted(nums)
-    processed = {}
-    idx = 0
-    while idx < len(nums):
-        val = nums[idx]
-        if val in processed or 0-val in processed:
-            idx += 1
-            continue
-        complements = twoSum(nums, idx+1, 0-val)
-        for c in complements:
-            c.insert(0, val)
-            item = tuple(c)
-            if item not in result:
-                result.append(item)
-        processed[val], processed[0-val] = True,  True
-        idx += 1
-    return result
-
-
-def twoSum(nums, start, target):
     result = []
-    l, r = start, len(nums)-1
-    while l < r:
-        diff = target-nums[l]
-        if diff == nums[r]:
-            result.append([nums[l], nums[r]])
-            l += 1
-            r -= 1
-        elif diff < nums[r]:
-            r -= 1
-        else:
-            l += 1
+    i = 0
+    for i in xrange(len(nums)):
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
+        l, r = i+1, len(nums)-1
+        diff = 0-nums[i]
+        while l < r:
+            if nums[l] + nums[r] == diff:
+                result.append((nums[i], nums[l], nums[r]))
+                while l < r:
+                    l += 1
+                    if l < r and nums[l] != nums[l-1]:
+                        break
+                while r > l:
+                    r -= 1
+                    if r > l and nums[r] != nums[r+1]:
+                        break
+            elif nums[l] + nums[r] < diff:
+                l += 1
+            else:
+                r -= 1
     return result
 
 
